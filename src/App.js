e011d3b4c9dbc1106ec1ab468c4496b80da55968
THIYAGARAJAN RAVICHANDRAN <thiyagarajanravichandran@gmail.com>
2023-07-04 15:23:50 +0000

    feat: Add ReduxToolKit page and components for managing movie and song playlists

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ButtonPage from './pages/ButtonPage';
 import ModalPage from './pages/ModalPage';
 import TablePage from './pages/TablePage';
 import CounterPage from './pages/CounterPage';
+import ReduxToolKit from './pages/ReduxToolKit';
 
 function App() {
   return (
@@ -29,6 +30,9 @@ function App() {
         </Route>
         <Route path="/counter">
           <CounterPage initialCount={10} />
+        </Route>
+        <Route path="/redux">
+          <ReduxToolKit />
         </Route>
       </div>
     </div>
---
+++ src/pages/ReduxToolKit.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+import MoviePlaylist from "./components/MoviePlaylist";
+import SongPlaylist from "./components/SongPlaylist";
+
+export default function ReduxToolKit() {
+  const handleResetClick = () => {
+    //
+  };
+
+  return (
+    <div className="container is-fluid">
+      <button onClick={() => handleResetClick()} className="button is-danger">
+        Reset Both Playlists
+      </button>
+      <hr />
+      <MoviePlaylist />
+      <hr />
+      <SongPlaylist />
+    </div>
+  );
+}
