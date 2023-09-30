d8434c8768ce3e760915f6fb0a747e72fb54a3fb
THIYAGARAJAN RAVICHANDRAN <thiyagarajanravichandran@gmail.com>
2023-09-30 15:02:39 +0000

    Updated Test Code

--- src/components/__test__/UserForm.test.js
+++ src/components/__test__/UserForm.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import user from '@testing-library/user-event';
-import UserForm from './UserForm';
+import UserForm from '../UserForm';
 
 test('it shows two inputs and a button', () => {
   // render the component
